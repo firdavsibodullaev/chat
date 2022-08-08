8aa0fda020291cbb99c9750710f628b3715870c4
Firdavs Ibodullaev <firdavs3253@gmail.com>
2022-08-08 10:35:54 +0500

    edited

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,7 +37,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-    wsPort: 6002,
+    wsPort: 6001,
     forceTLS: false,
     disableStats: true,
     enabledTransports: ['ws', 'wss']
